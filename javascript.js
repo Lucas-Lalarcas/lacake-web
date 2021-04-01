3fe375ffdf4b0cec0704cd34fd60b82666702094
Gustavo Alexandre <gustavoti717@gmail.com>
2021-03-31 22:30:49 -0300

    responsive

--- javascript.js
+++ javascript.js
@@ -54,8 +54,6 @@ else{
 
 
 
-
-
 //MASCARA JQUERY
 $('#cpf').mask("999.999.999-99");
 $('#telefone').mask('(99) 99999-9999');
@@ -65,52 +63,52 @@ $("#cep").mask("99.999-999");
 
 
 
-document.getElementById('olho').addEventListener('mousedown', function() {
-  document.getElementById('pass').type = 'text';
-});
+// document.getElementById('olho').addEventListener('mousedown', function() {
+//   document.getElementById('pass').type = 'text';
+// });
 
-document.getElementById('olho').addEventListener('mouseup', function() {
-  document.getElementById('pass').type = 'password';
-});
+// document.getElementById('olho').addEventListener('mouseup', function() {
+//   document.getElementById('pass').type = 'password';
+// });
 
-// Para que o password não fique exposto apos mover a imagem.
-document.getElementById('olho').addEventListener('mousemove', function() {
-  document.getElementById('pass').type = 'password';
-});
+// // Para que o password não fique exposto apos mover a imagem.
+// document.getElementById('olho').addEventListener('mousemove', function() {
+//   document.getElementById('pass').type = 'password';
+// });
 
 
   
-  //javascript da LATERALBAR//*
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+//   //javascript da LATERALBAR//*
+//   function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//   }
 
 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//   }
 
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+//   function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   }
 
 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+//   }
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
+//   function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+//   }
 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+//     document.body.style.backgroundColor = "white";
+//   }
 
